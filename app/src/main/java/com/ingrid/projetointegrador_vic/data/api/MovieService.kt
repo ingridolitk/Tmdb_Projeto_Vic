da84590e055d01a672b87889aad3c30e3d09dfe7
Ingrid da Silva Oliveira <ingrid.oliveiratk@gmail.com>
2021-08-22 14:15:12 -0300

    alteração layout

--- app/src/main/java/com/ingrid/projetointegrador_vic/data/api/MovieService.kt
+++ app/src/main/java/com/ingrid/projetointegrador_vic/data/api/MovieService.kt
@@ -1,11 +1,9 @@
 package com.ingrid.projetointegrador_vic.data.api
 
 import com.ingrid.projetointegrador_vic.domain.model.GenreResponse
-import com.ingrid.projetointegrador_vic.domain.model.Movie
 import com.ingrid.projetointegrador_vic.domain.model.MovieResult
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 interface MovieService {
 
@@ -15,6 +13,4 @@ interface MovieService {
     @GET("genre/movie/list?api_key=f7eaeb09d0bc520ae20d604476749d60")
     fun returnGenres(): Call<GenreResponse>
 
-   // @Query("SELECT * FROM Movie WHERE title = :title")
-    //fun returnTitle (title: String): Movie
 }
